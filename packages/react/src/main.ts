e0f05c5ee1fff61789167e92e1595e37d600b0c7
Thomas Groutars <tgroutars@gmail.com>
2023-01-31 17:10:59 +0100

    fix(react): export useUnrevealed

--- packages/react/src/main.ts
+++ packages/react/src/main.ts
@@ -1,3 +1,4 @@
 export { FeatureToggler } from './FeatureToggler';
 export { UnrevealedProvider } from './Provider';
 export type { UnrevealedProviderProps } from './Provider';
+export { useUnrevealed } from './useUnrevealed';
