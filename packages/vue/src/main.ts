6960a3fd1026926783b3fe61c7cf8d0687a7da4c
Thomas Groutars <tgroutars@gmail.com>
2023-05-15 16:05:20 +0200

    feat(vue): export injection keys

--- packages/vue/src/main.ts
+++ packages/vue/src/main.ts
@@ -1,3 +1,4 @@
+export { apiKeyInjectionKey, featuresInjectionKey } from './injectionKeys';
 export { UnrevealedPlugin } from './plugin';
 export type { FeatureKey, Features, TeamTraits, UserTraits } from './types';
 export { useFeature } from './useFeature';
