e997f3f1815b0614d17be40b6cd94e45aa6e6ed3
Thomas Groutars <tgroutars@gmail.com>
2023-09-14 23:05:22 +0200

    feat(react): expose array of features

--- packages/react/src/useFilteredFeatures.ts
+++ packages/react/src/useFilteredFeatures.ts
@@ -1,9 +1,16 @@
-import { useContext } from 'react';
+import { useCallback, useContext, useMemo } from 'react';
 import { UnrevealedContext } from './context';
 
 export function useFilteredFeatures() {
   const { allFeatures, filteredFeatures, setFilteredFeatures } =
     useContext(UnrevealedContext);
 
-  return { allFeatures, filteredFeatures, setFilteredFeatures };
+  return {
+    allFeatures: useMemo(() => [...allFeatures], [allFeatures]),
+    filteredFeatures: useMemo(() => [...filteredFeatures], [filteredFeatures]),
+    setFilteredFeatures: useCallback(
+      (features: string[]) => setFilteredFeatures(new Set(features)),
+      [setFilteredFeatures],
+    ),
+  };
 }
