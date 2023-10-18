2343cfb4dac02042c978d235168b71f741908997
Thomas Groutars <tgroutars@gmail.com>
2023-10-18 09:58:10 +0200

    feat(cli): read token from env var

--- packages/cli/src/auth/index.ts
+++ packages/cli/src/auth/index.ts
@@ -6,18 +6,27 @@ const dataDir = appDirs({ appName: 'unrevealed' }).data;
 const configFile = path.join(dataDir, 'config.json');
 
 export async function readToken(): Promise<string | null> {
-  try {
-    const { token } = await fs.readJSON(configFile);
-    if (typeof token !== 'string') {
-      return null;
-    }
+  const token = readTokenFromFile();
+  if (token) {
     return token;
-  } catch (err) {
-    return null;
   }
+
+  return process.env.UNREVEALED_ACCESS_TOKEN || null;
 }
 
 export async function writeToken(token: string) {
   await fs.ensureDir(dataDir);
   await fs.writeJSON(configFile, { token });
+}
+
+async function readTokenFromFile(): Promise<string | null> {
+  try {
+    let { token } = await fs.readJSON(configFile);
+    if (typeof token === 'string' && token) {
+      return token;
+    }
+    return null;
+  } catch (err) {
+    return null;
+  }
 }
