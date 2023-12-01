0779a6337361481767db304bb6659d40545f5bca
Thomas Groutars <tgroutars@gmail.com>
2023-12-01 12:03:35 +0100

    chore(serverless): add stage key type

--- packages/serverless/src/index.ts
+++ packages/serverless/src/index.ts
@@ -1,10 +1,12 @@
+export { UnrevealedClient, UnrevealedClientOptions } from './UnrevealedClient';
 export {
   FeatureAccess,
   FeatureKey,
   Features,
+  StageKey,
+  Stages,
   Team,
   TeamTraits,
   User,
   UserTraits,
 } from './types';
-export { UnrevealedClient, UnrevealedClientOptions } from './UnrevealedClient';
