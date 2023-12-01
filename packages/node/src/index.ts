794d530c64e90c7055edf32ff5d61bc77723e148
Thomas Groutars <tgroutars@gmail.com>
2023-12-01 12:09:09 +0100

    fix(node): export stage types

--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -1,13 +1,15 @@
 export type { UnrevealedLogger } from './Logger';
-export type {
-  FeatureAccess,
-  FeatureKey,
-  Features,
-  TeamTraits,
-  UserTraits,
-} from './types';
 export {
   UnrevealedClient,
   type ReadyState,
   type UnrevealedClientOptions,
 } from './UnrevealedClient';
+export type {
+  FeatureAccess,
+  FeatureKey,
+  Features,
+  StageKey,
+  Stages,
+  TeamTraits,
+  UserTraits,
+} from './types';
